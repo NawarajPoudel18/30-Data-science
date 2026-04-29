126dcaac44f6e8adc2c41aa998434172e2393e9b
nawaraj <nawarajpoudel18@gmail.com>
2026-04-29 13:30:54 +0545

    datascience day 8

--- day8.py
+++ day8.py
@@ -1,4 +1,6 @@
-from modules import c_to_f #this is the module being called here
+import modules #this is the module being called here
+from modules import c_to_f #this is the specific function being called from modules
+import modules as m #this is the modules being called and being inilised as m
 
 # print(modules.hello("nawaraj"))
 # print(modules.add(1,9))
@@ -6,7 +8,9 @@ from modules import c_to_f #this is the module being called here
 # print(modules.div(1,9))
 # print(modules.mul(1,9))
 
+print(modules.c_to_f(10))
 print(c_to_f(10))
+print(m.c_to_f(10))
 
 
 
