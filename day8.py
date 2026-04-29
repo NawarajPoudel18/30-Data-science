f998c456e7580fc700c9b9dd5e8109b85bffe6f4
nawaraj <nawarajpoudel18@gmail.com>
2026-04-29 13:02:43 +0545

    data science day 8

---
+++ day8.py
@@ -0,0 +1,7 @@
+import modules #this is the module being called here
+
+print(modules.hello("nawaraj"))
+print(modules.add(1,9))
+print(modules.sub(1,9))
+print(modules.div(1,9))
+print(modules.mul(1,9))
