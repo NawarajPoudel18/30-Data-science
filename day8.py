81c41f9628653a84bbd1672ea8c7f24b7d029d95
nawaraj <nawarajpoudel18@gmail.com>
2026-04-29 13:34:36 +0545

    datascience day 8

--- day8.py
+++ day8.py
@@ -1,5 +1,6 @@
 import modules #this is the module being called here
 from modules import c_to_f #this is the specific function being called from modules
+from modules import c_to_f as c #this is the specific function being called from modules and being inalized as c
 import modules as m #this is the modules being called and being inilised as m
 
 # print(modules.hello("nawaraj"))
@@ -8,9 +9,10 @@ import modules as m #this is the modules being called and being inilised as m
 # print(modules.div(1,9))
 # print(modules.mul(1,9))
 
-print(modules.c_to_f(10))
-print(c_to_f(10))
-print(m.c_to_f(10))
+print(modules.c_to_f(10)) # this is being called from "import modules"
+print(c_to_f(10)) #this is being called from "from modules import c_to_f"
+print(c(10)) #this is being called from "from modules import c_to_f as c"
+print(m.c_to_f(10))  #this is being called from "import modules as m"
 
 
 
