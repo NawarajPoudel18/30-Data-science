fd7e2764f2b113ea305459c2aca24ca73f8fbef7
Nawaraj Poudel <nawarajpoudel18@gmail.com>
2026-04-30 14:48:45 +0545

    Data science day eight

--- day8.py
+++ day8.py
@@ -7,7 +7,7 @@
 # # print(modules.add(1,9))
 # # print(modules.sub(1,9))
 # # print(modules.div(1,9))
-# # print(modules.mul(1,9))
+# # print(modules.mul(1,9)) 
 
 # print(modules.c_to_f(10)) # this is being called from "import modules"
 # print(c_to_f(10)) #this is being called from "from modules import c_to_f"
